802c84f39f188e7f777a207e9b7474702d553990
FliegendeWurst <arne.keller@posteo.de>
2025-05-24 10:22:58 +0200

    JMERule: remove unused methods

--- src/main/java/fr/up/xlim/sic/ig/jerboa/jme/model/JMERule.java
+++ src/main/java/fr/up/xlim/sic/ig/jerboa/jme/model/JMERule.java
@@ -8,14 +8,12 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 
 	protected JMEModeler modeler;
 	protected String name;
-	protected String comment;
 	protected String category;
 
 	protected JMEGraph left;
 	protected JMEGraph right;
 
 	protected boolean modified;
-	protected String header;
 	protected String precondition;
 	protected String preprocess;
 	protected String postprocess;
@@ -28,13 +26,10 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 		this.modeler = modeler;
 		this.name = name;
 
-		this.comment = "";
-
 		modified = false;
 
 		paramstopo = new ArrayList<>();
 
-		this.header = "";
 		category = "";
 		precondition = "";
 		preprocess = "";
@@ -47,22 +42,9 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 		right = new JMEGraph(this, false);
 	}
 
-	public String getFullName() {
-		String res = "";
-		if (category == null || "".equals(category))
-			res = getName();
-		else
-			res = getCategory() + "." + getName();
-		return res;
-	}
-
 	@Override
 	public String getName() {
 		return name;
-	}
-
-	public String getComment() {
-		return comment;
 	}
 
 	public JMEGraph getLeft() {
@@ -77,66 +59,6 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 		modeler = _modeler;
 	}
 
-	public void setName(String newname) {
-		if (newname != null && !newname.equals(name)) {
-			modified = true;
-			name = newname;
-		}
-	}
-
-	public void setComment(String newcomment) {
-		if (newcomment != null && !newcomment.equals(comment)) {
-			modified = true;
-			comment = newcomment;
-		}
-	}
-
-	public void setPrecondition(String text) {
-		if (text != null && !text.equals(precondition)) {
-			precondition = text;
-			modified = true;
-		}
-	}
-
-	public String getPrecondition() {
-		return precondition;
-	}
-
-	public void setHeader(String text) {
-		if (text != null && !text.equals(header)) {
-			header = text;
-			modified = true;
-		}
-	}
-
-	public String getHeader() {
-		return header;
-	}
-
-	public void setLeft(JMEGraph g) {
-		left = g;
-	}
-
-	public void setRight(JMEGraph g) {
-		right = g;
-	}
-
-	/**
-     * Return the first hook with the given name.
-     * Be careful if there are duplicates.
-     * 
-	 * @param name
-	 * @return
-	 */
-	public JMENode getHookNode(String name) {
-		for (JMENode n : left.getHooks()) {
-			if (n.getName().compareTo(name) == 0) {
-				return n;
-			}
-		}
-		return null;
-	}
-
 	@Override
 	public String toString() {
 		return getName();
@@ -146,39 +68,9 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 		return modeler;
 	}
 
-	public void setPreProcess(String text) {
-		if (text != null && !text.equals(preprocess)) {
-			preprocess = text;
-			modified = true;
-		}
-	}
-
-	public String getPreProcess() {
-		return preprocess;
-	}
-
-	public void setPostProcess(String text) {
-		if (text != null && !text.equals(postprocess)) {
-			postprocess = text;
-			modified = true;
-		}
-	}
-
-	public String getPostProcess() {
-		return postprocess;
-	}
-
 	public String getCategory() {
 		return category;
 	}
-
-	public void setCategory(String text) {
-		if (text != null && !text.equals(category)) {
-			this.category = text;
-			modified = true;
-		}
-	}
-
 
 	public List<JMEParamTopo> getParamsTopo() {
 		return paramstopo;
@@ -192,18 +84,6 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 		if (topo != null && !paramstopo.contains(topo)) {
 			modified = true;
 			paramstopo.add(topo);
-		}
-	}
-
-	public void insertParamTopo(JMEParamTopo p, int order) {
-		if (p != null && paramstopo.contains(p)) {
-			int oldorder = paramstopo.indexOf(p);
-			paramstopo.add(order, p);
-
-			if (oldorder >= order)
-				oldorder++;
-			modified = true;
-			paramstopo.remove(oldorder);
 		}
 	}
 
@@ -243,31 +123,4 @@ public abstract class JMERule implements JMEElement, Cloneable, Comparable<JMERu
 		paramstopo.clear();
 		paramstopo.addAll(newParam);
 	}
-	
-	/**
-	 * Try to add a hook to connected components without one (take any node
-	 * with full orbit).
-	 * @author romain
-	 */
-	public void enforceHooks() {
-		if (left != null)
-			left.enforceHooks();
-	}
-
-	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-	public String getMidProcess() {
-		return midprocess;
-	}
-
-	public void setMidProcess(String p) {
-		if (p != null && !p.equals(midprocess)) {
-			modified = true;
-			this.midprocess = p;
-		}
-	}
-	
-	public static final Pattern PATTERN_MODULE = Pattern.compile(
-			"([_]*[a-zA-Z][a-zA-Z0-9_]*([.][_]*[a-zA-Z][a-zA-Z0-9_]*)*)(<(.*)>)?\\*?\\z", Pattern.CASE_INSENSITIVE);
-	
 }
