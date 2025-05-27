65bd8b7776ebc0c1f07c64ed523e56620c0c9ae4
FliegendeWurst <arne.keller@posteo.de>
2025-05-27 09:25:57 +0200

    JMENode: JML specs

--- src/main/java/fr/up/xlim/sic/ig/jerboa/jme/model/JMENode.java
+++ src/main/java/fr/up/xlim/sic/ig/jerboa/jme/model/JMENode.java
@@ -13,14 +13,16 @@ import up.jerboa.core.JerboaOrbit;
  */
 public final class JMENode implements JMEElement {
 
+	//@ invariant name != null && orbit != null && kind != null;
+
 	// topological parameters
-	protected String name;
+	protected /*@ spec_public */ String name;
 	protected /*@ spec_public */ JerboaOrbit orbit;
-	private JMENodeKind kind;
+	private /*@ spec_public */ JMENodeKind kind;
 
 
 	// editor parameters
-	protected JMEGraph graph;
+	protected /*@ spec_public */ JMEGraph graph;
 
 
 	public JMENode(JMEGraph graph, String name, JMENodeKind k) {
@@ -31,6 +33,10 @@ public final class JMENode implements JMEElement {
 		this.kind = k;
 	}
 
+	/*@ public normal_behavior
+	  @ ensures \result == this.name;
+	  @ strictly_pure
+	  @*/
 	public String getName() {
 		return name;
 	}
@@ -43,15 +49,26 @@ public final class JMENode implements JMEElement {
 		return orbit;
 	}
 
+	/*@ public normal_behavior
+	  @ ensures \result == this.kind;
+	  @ strictly_pure
+	  @*/
 	public JMENodeKind getKind() {
 		return kind;
 	}
 
+	/*@ public normal_behavior
+	  @ ensures \result == this.graph;
+	  @ strictly_pure
+	  @*/
 	public JMEGraph getGraph(){
 		return graph;
 	}
-	
-	
+
+	/*@ public normal_behavior
+	  @ ensures (\forall int i; 0 <= i && i < \result.size(); \result.get(i) instanceof JMEArc);
+	  @ strictly_pure
+	  @*/
 	public List/*<JMEArc>*/ alphas() {
 		return (graph.getIncidentArcsFromNode(this));
 	}
