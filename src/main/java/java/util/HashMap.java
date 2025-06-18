3b19e2e76aadb9666f1b8e5c1250dd10e6f9a131
FliegendeWurst <arne.keller@posteo.de>
2025-06-18 10:04:40 +0200

    HashMap: constructor spec

--- src/main/java/java/util/HashMap.java
+++ src/main/java/java/util/HashMap.java
@@ -1,4 +1,11 @@
 package java.util;
 
-public class HashMap implements Map {
+public final class HashMap implements Map {
+    /*@ public normal_behavior
+      @ ensures \fresh(this) && \fresh(this.*) && \invariant_for(this);
+      @ ensures key_seq.length == 0;
+      @ ensures value_seq.length == 0;
+      @ assignable \nothing;
+      @*/
+    public HashMap();
 }
